66e50595134dc31bb167ffeaaf44bb78efc47d06
uswa <uswainam267@gmail.com>
2025-11-23 09:37:46 +0500

    added file

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -18,13 +18,14 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        $per_page = session( 'pagination_per_page' );
-        $per_page = ( ! empty( $per_page ) ) ? $per_page : 20;
-        $page     = ( ! empty( $_GET['page'] ) ) ? $_GET['page'] : 1;
-        $offset   = ( $page * $per_page ) - $per_page;
+        $per_page = session('pagination_per_page');
+        $per_page = (!empty($per_page)) ? $per_page : 20;
+        $page = (!empty($_GET['page'])) ? $_GET['page'] : 1;
+        $offset = ($page * $per_page) - $per_page;
 
-        $customers   = Customer::orderBy('id', 'DESC')->paginate( $per_page );
-        $title       = "Customer List";
+        $customers = Customer::orderBy('id', 'DESC')->paginate($per_page);
+        
+        $title = "Customer List";
         $description = "Some description for the page";
 
         return view('customer.list', compact('title', 'description', 'customers'));
@@ -50,38 +51,40 @@ class CustomerController extends Controller
      */
 
     public function store(Request $request)
-    {
-        $validators = Validator::make($request->all(), [
-            'name' => 'required',
-            'email' => 'required|email|unique:customers',
-            'phone' => 'required|numeric|unique:customers',
-            'gender' => 'required',
-            'profession' => 'required'
-        ]);
 
-        if ($validators->fails()) {
-            return redirect()->route('customer.create', app()->getLocale())->withErrors($validators)->withInput();
-        } else {
-            $customer = new Customer();
+         {
+            $validators = Validator::make($request->all(), [
+                'name' => 'required',
+                'email' => 'required|email|unique:customers',
+                'phone' => 'required|numeric|unique:customers',
+                'gender' => 'required',
+                'profession' => 'required'
+            ]);
 
-            if ( $request->hasFile('profile-picture') && $request->file('profile-picture')->isValid() ) {
-                $profile_picture = $request->file('profile-picture')->store('public');
-                $customer->profile_picture = $profile_picture;
+            if ($validators->fails()) {
+                return redirect()->route('customer.create', app()->getLocale())->withErrors($validators)->withInput();
+            } else {
+                $customer = new Customer();
+
+                if ($request->hasFile('profile-picture') && $request->file('profile-picture')->isValid()) {
+                    $profile_picture = $request->file('profile-picture')->store('public');
+                    $customer->profile_picture = $profile_picture;
+                }
+
+                $customer->name = $request->name;
+                $customer->email = $request->email;
+                $customer->phone = $request->phone;
+                $customer->gender = $request->gender;
+                $customer->profession = $request->profession;
+                $customer->address = $request->address;
+                $customer->status = $request->status;
+
+                $customer->save();
+
+                return redirect()->route('customer.list', app()->getLocale())->with('create', 'Customer created successfully !');
             }
-
-            $customer->name       = $request->name;
-            $customer->email      = $request->email;
-            $customer->phone      = $request->phone;
-            $customer->gender     = $request->gender;
-            $customer->profession = $request->profession;
-            $customer->address    = $request->address;
-            $customer->status     = $request->status;
-
-            $customer->save();
-
-            return redirect()->route('customer.list', app()->getLocale())->with('create', 'Customer created successfully !');
         }
-    }
+
 
     /**
      * Show the form for editing the specified resource.
@@ -92,8 +95,8 @@ class CustomerController extends Controller
 
     public function edit($language, $id)
     {
-        $title         = 'Edit Customer';
-        $description   = 'Some description for the page';
+        $title = 'Edit Customer';
+        $description = 'Some description for the page';
         $find_customer = Customer::where('id', $id)->get();
 
         return view('customer.edit', compact('title', 'description', 'find_customer'));
@@ -122,23 +125,23 @@ class CustomerController extends Controller
         } else {
             $customer = Customer::findOrFail($id);
 
-            if ( $request->hasFile('profile-picture') && $request->file('profile-picture')->isValid() ) {
+            if ($request->hasFile('profile-picture') && $request->file('profile-picture')->isValid()) {
                 $profile_picture = $request->file('profile-picture')->store('public');
                 $customer->profile_picture = $profile_picture;
             }
 
-            if ( $request->has('remove_profile_picture') && ! empty( $request->remove_profile_picture ) ) {
-                Storage::delete( $customer->profile_picture );
+            if ($request->has('remove_profile_picture') && !empty($request->remove_profile_picture)) {
+                Storage::delete($customer->profile_picture);
                 $customer->profile_picture = '';
             }
 
-            $customer->name       = $request->name;
-            $customer->email      = $request->email;
-            $customer->phone      = $request->phone;
-            $customer->gender     = $request->gender;
+            $customer->name = $request->name;
+            $customer->email = $request->email;
+            $customer->phone = $request->phone;
+            $customer->gender = $request->gender;
             $customer->profession = $request->profession;
-            $customer->address    = $request->address;
-            $customer->status     = $request->status;
+            $customer->address = $request->address;
+            $customer->status = $request->status;
 
             $customer->save();
 
