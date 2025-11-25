dfb5644c416551a3ce647ccb9db737cc89383bd0
uswa <uswainam267@gmail.com>
2025-11-25 19:09:15 +0500

    added  table

--- resources/views/admin/cart/create.blade.php
+++ resources/views/admin/cart/create.blade.php
@@ -2,4 +2,22 @@
  @section('title',$title)
  @section('description',$description)
  @section('content')
+ <div class="container-fluid">
+ <div class="row">
+        <div class="col-lg-12">
+            <div class="breadcrumb-main">
+                <h4 class="text-capitalize breadcrumb-title">{{ trans('menu.table-data') }}</h4>
+                <div class="breadcrumb-action justify-content-center flex-wrap">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="#"><i class="las la-home"></i>{{ trans('menu.table-menu-title') }}</a></li>
+                            <li class="breadcrumb-item"><a href="#">Apps</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">{{ trans('menu.table-data') }}</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
  @endsection
