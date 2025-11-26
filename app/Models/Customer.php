702f046f725ab89431682898d3cd2ef5a0decb4b
uswa <uswainam267@gmail.com>
2025-11-26 19:01:08 +0500

    added model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -5,8 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Customer extends Model {
+class Customer extends Model
+{
     use HasFactory;
 
-    protected $table = 'customers';
+    protected $fillable = [
+        'name',
+        'profile_picture',
+        'email',
+        'phone',
+        'gender',
+        'profession',
+        'status',
+        'address',
+    ];
+
+    protected $casts = [
+        'name'            => 'string',
+        'profile_picture' => 'string',
+        'email'           => 'string',
+        'phone'           => 'string',
+        'gender'          => 'string',
+        'profession'      => 'string',
+        'address'         => 'string',
+        'status'          => 'string', // active / inactive
+    ];
 }
