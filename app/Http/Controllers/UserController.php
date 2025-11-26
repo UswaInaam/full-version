6b45054e39db8e670bbd57607eb6ad490f1ee847
uswa <uswainam267@gmail.com>
2025-11-26 19:17:04 +0500

    added seeder

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,83 +3,103 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller {
-    
+
     /**
-     * Display user members of the resource.
-     *
-     * @return \Illuminate\View\View
+     * Display user members list.
      */
     public function index(){
         $title = "User Members";
         $description = "Some description for the page";
-        return view('pages.applications.user.member',compact('title','description'));
+
+        $users = User::orderBy('id', 'DESC')->paginate(20);
+
+        return view('pages.applications.user.member',
+            compact('title','description','users')
+        );
     }
 
     /**
-     * Display user grid of the resource.
-     *
-     * @return \Illuminate\View\View
+     * Display user grid.
      */
     public function grid(){
         $title = "User Grid";
         $description = "Some description for the page";
-        return view('pages.applications.user.grid',compact('title','description'));
+
+        $users = User::orderBy('id', 'DESC')->paginate(20);
+
+        return view('pages.applications.user.grid',
+            compact('title','description','users')
+        );
     }
 
     /**
-     * Display user list of the resource.
-     *
-     * @return \Illuminate\View\View
+     * Display user list.
      */
     public function list(){
         $title = "User List";
         $description = "Some description for the page";
-        return view('pages.applications.user.list',compact('title','description'));
+
+        $users = User::orderBy('id', 'DESC')->paginate(20);
+
+        return view('pages.applications.user.list',
+            compact('title','description','users')
+        );
     }
 
     /**
-     * Display user grid style of the resource.
-     *
-     * @return \Illuminate\View\View
+     * Display user grid style.
      */
     public function gridStyle(){
         $title = "User Grid Style List";
         $description = "Some description for the page";
-        return view('pages.applications.user.grid_style',compact('title','description'));
+
+        $users = User::orderBy('id', 'DESC')->paginate(20);
+
+        return view('pages.applications.user.grid_style',
+            compact('title','description','users')
+        );
     }
 
     /**
-     * Display user group of the resource.
-     *
-     * @return \Illuminate\View\View
+     * Display user group list.
      */
     public function userGroup(){
         $title = "User Group List";
         $description = "Some description for the page";
-        return view('pages.applications.user.user_group',compact('title','description'));
+
+        $users = User::orderBy('id', 'DESC')->paginate(20);
+
+        return view('pages.applications.user.user_group',
+            compact('title','description','users')
+        );
     }
 
     /**
-     * Display user add of the resource.
-     *
-     * @return \Illuminate\View\View
+     * Display add new user form.
      */
     public function add(){
         $title = "User Add";
         $description = "Some description for the page";
-        return view('pages.applications.user.add',compact('title','description'));
+
+        return view('pages.applications.user.add',
+            compact('title','description')
+        );
     }
 
     /**
-     * Display user table of the resource.
-     *
-     * @return \Illuminate\View\View
+     * Display user table.
      */
     public function table(){
         $title = "User Data Table";
         $description = "Some description for the page";
-        return view('pages.applications.user.data_table',compact('title','description'));
+
+        $users = User::orderBy('id', 'DESC')->paginate(20);
+
+        return view('pages.applications.user.data_table',
+            compact('title','description','users')
+        );
     }
 }
