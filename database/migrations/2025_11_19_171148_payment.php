6d32b320809cbdda7c62ff36bd00861bdb24a27b
uswa <uswainam267@gmail.com>
2025-11-27 22:07:37 +0500

    update migration

--- database/migrations/2025_11_19_171148_payment.php
+++ database/migrations/2025_11_19_171148_payment.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,7 +14,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger("order_id")->nullable();
             $table->string("payment_method");
-            $table->unsignedBigInteger("transaction_id")->nullable();
+            $table->string("transaction_id")->nullable();
 
             $table->decimal("amount", 10, 2);
             $table->string("status");
