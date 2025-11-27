98798f8349e8f999d575d585d36434e0c89f4c5f
uswa <uswainam267@gmail.com>
2025-11-27 22:08:12 +0500

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,25 +31,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // seed in dependency order
+        $this->call(UserSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(SubCategorySeeder::class);
         $this->call(ProductSeeder::class);
+        $this->call(ProductVariantSeeder::class);
+        $this->call(ProductImageSeeder::class);
+        $this->call(CouponSeeder::class);
+        $this->call(ShippingAddressSeeder::class);
+        $this->call(CustomerSeeder::class);
         $this->call(CartSeeder::class);
         $this->call(CartItemSeeder::class);
-        $this->call(CartSeeder::class);
-        $this->call(CategorySeeder::class);
-        $this->call(CouponSeeder::class);
         $this->call(OrderSeeder::class);
         $this->call(OrderItemSeeder::class);
         $this->call(PaymentSeeder::class);
-        $this->call(ProductImageSeeder::class);
-        $this->call(ProductVariantSeeder::class);
         $this->call(ReviewSeeder::class);
-        $this->call(SettingSeeder::class);
-        $this->call(ShippingAddressSeeder::class);
-        $this->call(SubCategorySeeder::class);
         $this->call(WishlistSeeder::class);
-        $this->call(CustomerSeeder::class);
-        $this->call(UserSeeder::class);
+        $this->call(SettingSeeder::class);
 
     }
 }
