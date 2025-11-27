46c0c8a11ef6d63f9609abfaabaa65bf0eb540f4
uswa <uswainam267@gmail.com>
2025-11-27 15:53:13 +0500

    added foreign

--- database/migrations/2025_11_19_170906_cart_item.php
+++ database/migrations/2025_11_19_170906_cart_item.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('cart_id');
-            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('cart_id')->nullable();
+            $table->unsignedBigInteger('product_id')->nullable();
             $table->unsignedBigInteger('variant_id')->nullable();
             $table->integer('quantity')->default(1);
             $table->decimal('price', 10, 2);
