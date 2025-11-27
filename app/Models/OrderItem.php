9921ea9ce900dade18c6a951d6385bc253717075
uswa <uswainam267@gmail.com>
2025-11-27 10:59:16 +0500

    define relations

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -25,4 +25,18 @@ class OrderItem extends Model
         'price'      => 'decimal:2',
         'subtotal'   => 'decimal:2',
     ];
+    
+    public function order() {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function variant() {
+        return $this->belongsTo(ProductVariant::class);
+    }
 }
+
+
